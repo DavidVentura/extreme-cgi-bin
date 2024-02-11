fe7d5affec740e1fb35e70567552dd57135505af
David Ventura <davidventura27@gmail.com>
2024-02-11 23:13:06 +0100

    refactor

--- vm_runner/src/main.rs
+++ vm_runner/src/main.rs
@@ -2,109 +2,24 @@ mod tap;
 mod tcp_proxy;
 mod vm;
 
-use lazy_static::lazy_static; // 1.4.0
-use std::sync::Mutex;
+use vm::VmNetCfg;
 
 use std::net::{Ipv4Addr, TcpListener};
-use std::thread;
-
-use std::time::Instant;
-
-#[derive(Clone)]
-pub(crate) struct VmNetCfg {
-    vm_ip: Ipv4Addr,
-    tap_ip: Ipv4Addr,
-    netmask: Ipv4Addr,
-    tap_iface: String,
-    vm_mac: String,
-}
-
-fn first_free_vm_idx(max: usize, free: &[bool]) -> Option<usize> {
-    for i in 0..max {
-        if free[i] {
-            return Some(i);
-        }
-    }
-    None
-}
-
-lazy_static! {
-    static ref VM_FREE: Mutex<Vec<bool>> = Mutex::new(vec![]);
-}
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     let max_concurrent_vms: usize = 20;
     let subnet = Ipv4Addr::new(172, 16, 0, 0);
-    let netmask = Ipv4Addr::new(255, 255, 255, 252);
-    let gw_oct = subnet.octets();
-    let ip_oct = subnet.octets();
-
-    let mut vm_net_cfg: Vec<VmNetCfg> = Vec::new();
-    for j in 0..max_concurrent_vms {
-        let tap_ip = Ipv4Addr::new(
-            ip_oct[0],
-            ip_oct[1],
-            ip_oct[2],
-            ip_oct[3] + (j as u8) * 4 + 0,
-        );
-        let vm_ip = Ipv4Addr::new(
-            gw_oct[0],
-            gw_oct[1],
-            gw_oct[2],
-            gw_oct[3] + (j as u8) * 4 + 1,
-        );
-        let tap_name =
-            tap::add_tap(j as u16, tap_ip, netmask).expect("Failed to create a TAP device");
-
-        vm_net_cfg.push(VmNetCfg {
-            vm_ip,
-            tap_ip,
-            netmask,
-            tap_iface: tap_name,
-            vm_mac: format!("06:00:AC:10:00:{j:02x}"),
-        });
-        VM_FREE.lock().unwrap().push(true);
-    }
+    vm::populate_vm_configs(max_concurrent_vms, subnet);
 
     for stream in listener.incoming() {
         match stream {
-            Ok(inc) => {
-                let req_start = Instant::now();
-
-                let free_vm_idx =
-                    match first_free_vm_idx(max_concurrent_vms, &VM_FREE.lock().unwrap()) {
-                        None => {
-                            println!("no free vms");
-                            continue;
-                        }
-                        Some(i) => i,
-                    };
-                let this_vm_cfg = vm_net_cfg[free_vm_idx].clone();
-                let this_vm_cfg2 = this_vm_cfg.clone();
-
-                VM_FREE.lock().unwrap()[free_vm_idx] = false;
-
-                let threads = vec![
-                    thread::spawn(move || {
-                        vm::make_vm(this_vm_cfg).unwrap();
-                    }),
-                    thread::spawn(move || {
-                        let cstr = vm::connect_to_vm(this_vm_cfg2.vm_ip);
-                        tcp_proxy::splice(inc, cstr);
-                        println!("Request done in {:?}", req_start.elapsed());
-                    }),
-                ];
-
-                thread::spawn(move || {
-                    for t in threads {
-                        t.join().unwrap();
-                    }
-                    VM_FREE.lock().unwrap()[free_vm_idx] = true;
-                });
-            }
-            Err(_) => {
-                println!("Error");
+            Ok(inc) => match VmNetCfg::handle_tcp_conn(inc) {
+                Err(e) => println!("Error creating VM: {:?}", e),
+                Ok(_) => (),
+            },
+            Err(e) => {
+                println!("Error with the stream: {:?}", e);
             }
         }
     }
--- vm_runner/src/vm.rs
+++ vm_runner/src/vm.rs
@@ -1,8 +1,10 @@
-use crate::VmNetCfg;
+use crate::tcp_proxy;
+use lazy_static::lazy_static;
+use std::error::Error;
 use std::net::{Ipv4Addr, SocketAddr, TcpStream};
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-
 use std::time::Instant;
 use vmm::builder::{build_and_boot_microvm, StartMicrovmError};
 use vmm::resources::VmResources;
@@ -17,19 +19,116 @@ pub enum UtilsError {
     /// Failed to build microVM: {0}
     BuildMicroVm(#[from] StartMicrovmError),
 }
-pub(crate) fn make_vm(cfg: VmNetCfg) -> Result<(), UtilsError> {
-    let start = Instant::now();
 
-    let instance_info = InstanceInfo {
-        id: "anonymous-instance".to_string(),
-        state: VmState::NotStarted,
-        vmm_version: "Amazing version".to_string(),
-        app_name: "cpu-template-helper".to_string(),
-    };
+lazy_static! {
+    // How do you do this properly???
+    static ref VMS: Mutex<Vec<VmNetCfg>> = Mutex::new(vec![]);
+}
 
-    let boot_args = format!("panic=-1 reboot=t quiet ip.dev_wait_ms=0 root=/dev/vda ip={0}::{1}:{2}:hostname:eth0:off init=/init", cfg.vm_ip, cfg.tap_ip, cfg.netmask);
-    let config = format!(
-        r#"
+fn first_free_vm_idx(vms: &[VmNetCfg]) -> Option<usize> {
+    for i in 0..vms.len() {
+        if vms[i].free {
+            return Some(i);
+        }
+    }
+    None
+}
+fn nth_ip_in_subnet(subnet: Ipv4Addr, n: u8) -> Ipv4Addr {
+    let ip_oct = subnet.octets();
+    Ipv4Addr::new(ip_oct[0], ip_oct[1], ip_oct[2], ip_oct[3] + n)
+}
+
+pub(crate) fn populate_vm_configs(len: usize, subnet: Ipv4Addr) {
+    let netmask = Ipv4Addr::new(255, 255, 255, 252);
+
+    for j in 0..len {
+        let tap_ip = nth_ip_in_subnet(subnet, (j as u8) * 4 + 0);
+        let vm_ip = nth_ip_in_subnet(subnet, (j as u8) * 4 + 1);
+        let vm_mac = format!("06:00:AC:10:00:{j:02x}");
+        let tap_name =
+            crate::tap::add_tap(j as u16, tap_ip, netmask).expect("Failed to create a TAP device");
+
+        VMS.lock().unwrap().push(VmNetCfg {
+            vm_ip,
+            tap_ip,
+            netmask,
+            vm_mac,
+            tap_iface: tap_name,
+            free: true,
+        });
+    }
+}
+#[derive(Clone)]
+pub(crate) struct VmNetCfg {
+    vm_ip: Ipv4Addr,
+    tap_ip: Ipv4Addr,
+    netmask: Ipv4Addr,
+    tap_iface: String,
+    vm_mac: String,
+    free: bool,
+}
+
+impl VmNetCfg {
+    pub(crate) fn handle_tcp_conn(inc: TcpStream) -> Result<(), Box<dyn Error>> {
+        let idx = match first_free_vm_idx(&VMS.lock().unwrap()) {
+            None => {
+                return Err("no free vms, no request for you".into());
+            }
+            Some(i) => i,
+        };
+        let req_start = Instant::now();
+        let this = VMS.lock().unwrap()[idx].clone();
+        println!("Am handling as vmm {}", idx);
+        VMS.lock().unwrap()[idx].free = false;
+
+        let clone = this.clone();
+        thread::spawn(move || {
+            this.make().expect("Could not create VM");
+            VMS.lock().unwrap()[idx].free = true;
+        });
+
+        thread::spawn(move || {
+            let cstr = clone.connect();
+            tcp_proxy::splice(inc, cstr);
+            println!("Request done in {:?}, idx: {}", req_start.elapsed(), idx);
+        });
+        Ok(())
+    }
+
+    fn connect(&self) -> TcpStream {
+        let start = Instant::now();
+        thread::sleep(Duration::from_millis(5));
+        loop {
+            let sr = TcpStream::connect_timeout(
+                &SocketAddr::new(self.vm_ip.into(), 8081),
+                Duration::from_millis(1),
+            );
+            match sr {
+                Ok(sr) => {
+                    println!("is up - {:?}", start.elapsed());
+                    return sr;
+                }
+                Err(_) => {
+                    thread::sleep(Duration::from_millis(1));
+                }
+            }
+        }
+    }
+
+    fn make(&self) -> Result<(), UtilsError> {
+        let start = Instant::now();
+
+        let instance_info = InstanceInfo {
+            id: "anonymous-instance".to_string(),
+            state: VmState::NotStarted,
+            vmm_version: "Amazing version".to_string(),
+            app_name: "cpu-template-helper".to_string(),
+        };
+
+        let boot_args = format!("panic=-1 reboot=t quiet ip.dev_wait_ms=0 root=/dev/vda ip={0}::{1}:{2}:hostname:eth0:off init=/init", self.vm_ip, self.tap_ip, self.netmask);
+        // TODO: figure out how to pass a real config and not json :^)
+        let config = format!(
+            r#"
 {{
   "boot-source": {{
     "kernel_image_path": "/home/david/git/lk/vmlinux-mini-net",
@@ -38,7 +137,7 @@ pub(crate) fn make_vm(cfg: VmNetCfg) -> Result<(), UtilsError> {
   "machine-config": {{
     "vcpu_count": 1,
     "backed_by_hugepages": true,
-    "mem_size_mib": 128
+    "mem_size_mib": 32
   }},
   "drives": [{{
     "drive_id": "rootfs",
@@ -52,53 +151,35 @@ pub(crate) fn make_vm(cfg: VmNetCfg) -> Result<(), UtilsError> {
     "host_dev_name": "{1}"
   }}]
 }}"#,
-        cfg.vm_mac, cfg.tap_iface
-    );
-    let mut vm_resources =
-        VmResources::from_json(&config, &instance_info, HTTP_MAX_PAYLOAD_SIZE, None)
-            .map_err(UtilsError::CreateVmResources)?;
-    vm_resources.boot_timer = false;
+            self.vm_mac, self.tap_iface
+        );
+        let mut vm_resources =
+            VmResources::from_json(&config, &instance_info, HTTP_MAX_PAYLOAD_SIZE, None)
+                .map_err(UtilsError::CreateVmResources)?;
+        vm_resources.boot_timer = false;
 
-    let mut event_manager = EventManager::new().unwrap();
-    let seccomp_filters = get_empty_filters();
+        let mut event_manager = EventManager::new().unwrap();
+        let seccomp_filters = get_empty_filters();
 
-    let vm = build_and_boot_microvm(
-        &instance_info,
-        &vm_resources,
-        &mut event_manager,
-        &seccomp_filters,
-    )
-    .unwrap();
-    let elapsed = start.elapsed();
-    println!("Time to start VM: {:?}", elapsed);
-    loop {
-        event_manager.run().unwrap();
-        match vm.lock().unwrap().shutdown_exit_code() {
-            Some(FcExitCode::Ok) => break,
-            Some(_) => {
-                println!("vm died??");
-                return Ok(());
-            }
-            None => continue,
-        }
-    }
-    Ok(())
-}
-
-pub(crate) fn connect_to_vm(ip: Ipv4Addr) -> TcpStream {
-    let start = Instant::now();
-    thread::sleep(Duration::from_millis(5));
-    loop {
-        let sr =
-            TcpStream::connect_timeout(&SocketAddr::new(ip.into(), 8081), Duration::from_millis(1));
-        match sr {
-            Ok(sr) => {
-                println!("is up - {:?}", start.elapsed());
-                return sr;
-            }
-            Err(_) => {
-                thread::sleep(Duration::from_millis(1));
+        let vm = build_and_boot_microvm(
+            &instance_info,
+            &vm_resources,
+            &mut event_manager,
+            &seccomp_filters,
+        )?;
+        let elapsed = start.elapsed();
+        println!("Time to start VM: {:?}", elapsed);
+        loop {
+            event_manager.run().unwrap();
+            match vm.lock().unwrap().shutdown_exit_code() {
+                Some(FcExitCode::Ok) => break,
+                Some(_) => {
+                    println!("vm died??");
+                    return Ok(());
+                }
+                None => continue,
             }
         }
+        Ok(())
     }
 }
