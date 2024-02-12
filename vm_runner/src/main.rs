cd3955c2d7643bbb5f69ae18a287074e9efe75d1
David Ventura <davidventura27@gmail.com>
2024-02-12 14:56:47 +0100

    bump openfiles to 2k to run 63 concurrent vms

--- vm_runner/src/main.rs
+++ vm_runner/src/main.rs
@@ -11,11 +11,18 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
-    let max_concurrent_vms: u8 = 20;
+    let max_concurrent_vms: u8 = 63;
     let subnet = Ipv4Addr::new(172, 16, 0, 0);
 
-    let handler = VmHandler::new(max_concurrent_vms, subnet).unwrap();
-
+    rlimit::Resource::NOFILE.set(2048, 2048);
+    let handler = match VmHandler::new(max_concurrent_vms, subnet) {
+        Ok(h) => h,
+        Err(e) => {
+            println!("Failed to create handler: {}", e);
+            std::process::exit(1);
+        }
+    };
+    println!("Listening for connections");
     thread::scope(|s| {
         for stream in listener.incoming() {
             match stream {
